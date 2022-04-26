3de1ea3e229cfc5284456c1bb72ab645cb4f4433
胡哲诚 <71585442+cloudhzc@users.noreply.github.com>
2022-04-26 20:27:11 +0800

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,28 @@
-let a = 0
-let b = 0
+let player1score = 0
+let player2score = 0
 basic.showString("Scoreboard")
 basic.forever(function () {
-    basic.showString((a)+':'+(b))
+    basic.showString((player1score)+':'+(player2score))
     basic.clearScreen()
-    basic.pause(1000)
+    basic.pause(950)
 })
 input.onButtonPressed(Button.A, function () {
     control.inBackground(function() {
-        a++
+        player1score++
+        //按钮A按下时玩家1分数+1
     })
 })
 input.onButtonPressed(Button.B, function () {
     control.inBackground(function() {
-        b++
+        player2score++
+        //按钮B按下时玩家1分数+1
     })
 })
 input.onButtonPressed(Button.AB, function () {
     control.inBackground(function() {
-        a = 0
-        b = 0
+        player1score = 0
+        player2score = 0
+        //按钮A+B按下时玩家1、2分数清零
     })
     basic.showString("New-round!")
 })
